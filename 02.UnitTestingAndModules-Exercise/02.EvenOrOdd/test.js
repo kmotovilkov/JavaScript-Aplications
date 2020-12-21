18c0f748ff87cdde98e3673217ebb1e959d1d900
kmotovilkov <monyllakk@abv.bg>
2020-12-21 21:48:32 +0200

    update

---
+++ 02.UnitTestingAndModules-Exercise/02.EvenOrOdd/test.js
@@ -0,0 +1,14 @@
+let assert = require('chai').assert;
+let app = require('./isOddOrEven');
+
+describe('Is odd or even', function () {
+    it('should return undefined with param different from string', function () {
+        assert.equal(undefined, app.isOddOrEven(5));
+    });
+    it('should return even', function () {
+        assert.equal('even', app.isOddOrEven('words'));
+    });
+    it('should return odd', function () {
+        assert.equal('odd', app.isOddOrEven('words'));
+    });
+});
