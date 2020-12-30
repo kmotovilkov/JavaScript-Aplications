b90b26b36de4ed7fd529c86c157b7d6fbe825036
kmotovilkov <monyllakk@abv.bg>
2020-12-30 09:10:40 +0200

    update

--- 02.UnitTestingAndModules-Exercise/02.EvenOrOdd/test.js
+++ 02.UnitTestingAndModules-Exercise/02.EvenOrOdd/test.js
@@ -6,7 +6,7 @@ describe('Is odd or even', function () {
         assert.equal(undefined, app.isOddOrEven(5));
     });
     it('should return even', function () {
-        assert.equal('even', app.isOddOrEven('words'));
+        assert.equal('even', app.isOddOrEven('word'));
     });
     it('should return odd', function () {
         assert.equal('odd', app.isOddOrEven('words'));
---
+++ 02.UnitTestingAndModules-Exercise/03.CharLookup/test.js
@@ -0,0 +1,22 @@
+let assert = require('chai').assert;
+let lookupChar = require('./lookUpChar').lookupChar;
+
+describe('lookUpChar', function () {
+    it('should return undefined with incorrect first param', function () {
+assert.equal(undefined, lookupChar(5,0));
+    });
+    it('should return undefined with incorrect second param', function () {
+        assert.equal(undefined, lookupChar('asd','dsa'));
+    });
+    it('should return incorrect index with first param length', function () {
+        assert.equal('Incorrect index', lookupChar('asd',4));
+    });
+    it('should return incorrect index with second param lower then 0', function () {
+        assert.equal('Incorrect index', lookupChar('asd',-1));
+    });
+    it('should return correct character', function () {
+        assert.equal('a', lookupChar('dfaff',2));
+    });
+
+
+});
