3f389f2154b30985bde68133c37e5a793b2c91a6
kmotovilkov <monyllakk@abv.bg>
2021-01-07 20:08:16 +0200

    update

--- 04.RestServicesAndAJAX-Exercise/02.BUS-SCHEDULE/app.js
+++ 04.RestServicesAndAJAX-Exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,39 @@
 function solve() {
+    const baseURL = 'https://judgetests.firebaseio.com/schedule/';
+    const info = document.getElementById('info');
+    let stopName;
+    let stopID = 'depot';
+
+    function changeBtn() {
+        let departBtn = document.getElementById('depart');
+        let arivetBtn = document.getElementById('arrive');
+        if (departBtn.disabled) {
+            departBtn.disabled = false;
+            arivetBtn.disabled = true;
+        } else {
+            departBtn.disabled = true;
+            arivetBtn.disabled = false;
+        }
+    }
 
     function depart() {
-        console.log('Depart TODO...');
+        const url = baseURL + stopID + '.json';
+        fetch(url)
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        info.innerText = `Next stop ${data.name}`;
+                        stopID = data.next;
+                        stopName = data.name;
+                        info.style.fontSize = '1.2em';
+                    });
+            });
+        changeBtn();
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.innerText = `Arriving ${stopName}`;
+        changeBtn();
     }
 
     return {
