ebaf8f52a445e4fc0140477d55fb5dcb1d3da648
kmotovilkov <monyllakk@abv.bg>
2021-02-01 21:43:22 +0200

    update

---
+++ 07.Templating/Demo/articles.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        title: "First Article",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis culpa est facere incidunt magni odit reiciendis voluptatum. Cum ea expedita in repellendus similique vel. Eos fugit illo quaerat quia.",
+        author: "Asdf Nhjkl"
+    },
+    {
+        title: "Second Article",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis culpa est facere incidunt magni odit reiciendis voluptatum. Cum ea expedita in repellendus similique vel. Eos fugit illo quaerat quia.",
+        author: "Bdsfdf Gerwerkl"
+    },
+    {
+        title: "Third Article",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis culpa est facere incidunt magni odit reiciendis voluptatum. Cum ea expedita in repellendus similique vel. Eos fugit illo quaerat quia.",
+        author: ""
+    },
+];
---
+++ 07.Templating/Demo/create-article.js
@@ -0,0 +1,14 @@
+const createArticle = (data) => `
+ <article >
+            <header>
+                <h3>${data.title}</h3>
+                <section>
+                    <p>${data.body}</p>
+                </section>
+                <footer>
+                    <p>Author: ${data.author}</p>
+                </footer>
+            </header>
+`;
+
+export default createArticle;
