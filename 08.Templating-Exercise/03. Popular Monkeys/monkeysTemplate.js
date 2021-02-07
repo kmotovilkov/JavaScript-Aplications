40c933f008fb7dfec83908f7744c9121bd46a9c3
kmotovilkov <monyllakk@abv.bg>
2021-02-07 19:48:02 +0200

    update

--- 08.Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
+++ 08.Templating-Exercise/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,30 @@
-$(() => {
-    // TODO
-})
+import monkeys from "./monkeys.js";
+
+
+const allMonkeys = document.querySelector("div.monkeys");
+
+fetch("./monkeys.hbs")
+    .then(res => res.text())
+    .then(monkeyTempSrc => {
+        const monkeysTemp = Handlebars.compile(monkeyTempSrc);
+        const resultHtml = monkeysTemp({monkeys});
+        allMonkeys.innerHTML = resultHtml;
+
+        attachEvent();
+    }).catch(e => console.error(e));
+
+function attachEvent() {
+    allMonkeys.addEventListener("click", (e) => {
+        const {target} = e;
+        if (target.nodeName !== "BUTTON" || target.textContent !== "Info") {
+            return;
+        }
+        const p = target.parentNode.querySelector("p");
+
+        if (p.style.display === "none" ) {
+            p.style.display = "block";
+        } else {
+            p.style.display = "none";
+        }
+    });
+}
